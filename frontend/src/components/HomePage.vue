764770901b779c060d7837e0038de8f14137320f
chris <sweetchris1196@gmail.com>
2024-09-17 14:29:04 -0400

    router direct

---
+++ frontend/src/components/HomePage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container mt-5">
+      <h1>Welcome to the Home Page</h1>
+      <p>Here you can see your posts and explore the blog.</p>
+  
+      <div class="mt-4">
+        <button class="btn btn-primary" @click="createPost">Create New Post</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage',
+    methods: {
+      createPost() {
+        this.$router.push('/create-post');  // Navigate to create post page
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #007bff;
+  }
+  </style>
+  
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import RegisterForm from './components/RegisterForm.vue'
 import LoginForm from './components/LoginForm.vue'
+import HomePage from './components/HomePage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
@@ -10,6 +11,7 @@ import 'bootstrap'
 const routes = [
   { path: '/register', component: RegisterForm },
   { path: '/login', component: LoginForm },
+  { path: '/home', component: HomePage },
 ]
 
 const router = createRouter({
